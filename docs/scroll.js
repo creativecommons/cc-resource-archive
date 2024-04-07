01ba74f34d3e21bb454e2370be52269547cdb6b2
Sam <paarthsamarth@gmail.com>
2024-04-07 12:37:04 +0530

    Revised code
    Edited the anchor tag, added a span tag and linked the anchor tag to it.
    Also, revised the script.

--- docs/scroll.js
+++ docs/scroll.js
@@ -1,30 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     let stt = document.getElementById("stt");
-    window.onscroll = function() {scrollFunction()};
-    
-    function scrollFunction() {
-        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+
+    window.onscroll = function() {
+        if (window.scrollY > 400) {
             stt.style.display = "block";
         } else {
             stt.style.display = "none";
         }
-    }
-})
-
-    // Get a reference to the "Back To Top" button
-    let sttButton = document.getElementById("stt");
-
-    // Add a click event listener to the button
-    sttButton.addEventListener("click", function(event) {
-        // Prevent default anchor behavior
-        event.preventDefault();
-
-        // Get a reference to the target element with the ID "resourcenavbar"
-        let targetElement = document.getElementById("global-header");
-
-        // Check if the target element exists
-        if (targetElement) {
-            // Scroll to the target element
-            targetElement.scrollIntoView({ behavior: "smooth" });
-        }
+    };
 });
