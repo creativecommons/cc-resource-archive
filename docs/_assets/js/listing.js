1003d4d7dc3fd21c017e2c19cd030a3f780e76f3
Murdock9803 <ayushsahu9803@gmail.com>
2024-07-02 23:27:53 +0530

    updated the getUrlVars function with modern javascript

--- docs/_assets/js/listing.js
+++ docs/_assets/js/listing.js
@@ -2,19 +2,20 @@
 // The all.html page is based on the layout listing.html
 
 
+// Function to get the selected filters for Topic, Medium and Language
 function getUrlVars() {
-    var vars = [],
-      hash;
-    var hashes = window.location.href
-      .slice(window.location.href.indexOf("?") + 1)
-      .split("&");
-    for (var i = 0; i < hashes.length; i++) {
-      hash = hashes[i].split("=");
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1];
-    }
+    const vars = {};
+    const filterString = window.location.search.slice(1);  // slice(1) removes the "?" from window.location.search
+    const hashes = filterString.split("&");
+
+    hashes.forEach(hash => {
+        const [key, value] = hash.split("=");
+        vars[key] = value;
+    });
+
     return vars;
-  }
+}
+
 
   document.write("<style>.thumbnailbox { display: none; }</style>");
 
